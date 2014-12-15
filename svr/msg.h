5ce228b65ff01773a8365fe4c03c41ec49e09075
arsee11 <arsee@sina.com>
2014-12-15 09:59:03 +0800

    add svr/

--- svr/msg.h
+++ svr/msg.h
@@ -10,20 +10,6 @@ using namespace std;
 #include "mvcobject.h"
 #endif
 
-class Member;
-
-class Msg
-{
-public:
-	Member *From();
-	Member *To();
-	string Context();
-
-private:
-	Member *_from, *_to;
-	string _context;
-
-};
 
 typedef arsee::Object<Msg> msg_obj_t;
 typedef shared_ptr<msg_obj_t> msg_ptr_t;
